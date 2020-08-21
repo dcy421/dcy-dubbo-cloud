bff314a554726e78a4c267623df5c1895456ec3c
dongchunyu <1540770111@qq.com>
2020-08-21 11:12:49 +0800

    优化代码

--- cloud-gateway/src/main/java/com/dcy/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/dcy/gateway/GatewayApplication.java
@@ -2,13 +2,11 @@ package com.dcy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author dcy
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
